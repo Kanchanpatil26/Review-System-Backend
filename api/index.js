9d508ccafaa58645ebd445c9820119cc1e471476
Kanchan Patil <161240351+Kanchanpatil26@users.noreply.github.com>
2024-11-15 15:21:45 +0530

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import dbConnect from "../config/database.js";
+import User from "../models/user.js";
+import router from "../routes/feedbackRoutes.js";
+
+
+const app=express();
+
+const PORT=3000;
+
+app.use(express.json())
+app.use("/api/v1",router)
+
+dotenv.config();
+
+dbConnect();
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running at port:${PORT}`);
+})
